2a4bf8e2034f0103a053e06bfb18ba3210920ca8
Flunt1k <chvp00@gmail.com>
2020-10-09 12:14:01 +0300

    add routing and template for controller

---
+++ controllers/WashingMachineController.ts
@@ -0,0 +1,44 @@
+class WashingMachineController {
+  getAll() {
+
+  }
+
+  getMachineBySerialNumber() {
+
+  }
+
+  getMachinesByModel() {
+
+  }
+
+  getMachinesByStatus() {
+
+  }
+
+  addNewMachine() {
+
+  }
+
+  removeMachineBySerialNumber() {
+
+  }
+
+  removeMachineByModel() {
+
+  }
+
+  updateMachineBySerialNumber() {
+
+  }
+  updateMachinesByModel() {
+
+  }
+
+  updateMachineStatusBySerialNumber() {
+
+  }
+
+
+}
+
+export const WashingMachineCtrl = new WashingMachineController()
---
+++ routes/washingMachine.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import {WashingMachineCtrl} from '../controllers/WashingMachineController'
+
+export const router: express.Router = express.Router()
+
+router.get('/', WashingMachineCtrl.getAll)
+router.get('/model', WashingMachineCtrl.getMachinesByModel)
+router.get('/status', WashingMachineCtrl.getMachinesByStatus)
+router.get('/:serialNumber', WashingMachineCtrl.getMachineBySerialNumber)
+
+router.post('/', WashingMachineCtrl.addNewMachine)
+
+router.delete('/model', WashingMachineCtrl.removeMachineByModel)
+router.delete('/:serialNumber', WashingMachineCtrl.removeMachineBySerialNumber)
+
+router.patch('/model', WashingMachineCtrl.updateMachinesByModel)
+router.patch('/status', WashingMachineCtrl.updateMachineStatusBySerialNumber)
+router.patch('/:serialNumber', WashingMachineCtrl.updateMachineBySerialNumber)
