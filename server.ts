6c7a0e0de09e9471b0fb0948ed672734f1fc8ac4
Flunt1k <chvp00@gmail.com>
2020-10-10 11:06:58 +0300

    refactroing server

--- server.ts
+++ server.ts
@@ -9,16 +9,3 @@ const server = http.createServer(app)
 server.listen(PORT, (): void => {
   console.log('server is working on', PORT)
 })
-
-process.on('uncaughtException', (err: Error): void => {
-  console.error('Server RUINED\n', err)
-  process.exit(1)
-})
-
-process.on('SIGINT', () => {
-  console.log('Server has been stopped')
-  server.close((err: Error | undefined) => {
-    if (err) console.warn('Error ', err)
-  })
-  process.exit(0)
-})
