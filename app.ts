246f8becd5e1062b83a61a883502e90e92a85e01
Flunt1k <chvp00@gmail.com>
2020-10-12 02:57:24 +0300

    preparing to release

--- app.ts
+++ app.ts
@@ -11,10 +11,12 @@ export const app: express.Application = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(logger('dev'))
-app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.use('/api/washingMachine', washingMachineRouter)
 
-app.get('*', (_:any, res:express.Response) => {
-  res.sendFile(__dirname + '/client/build/index.html')
-})
+if (process.env.MODE === 'production') {
+app.use(express.static(path.join(__dirname, '..', 'client/build')))
+  app.get('*', (_:any, res:express.Response) => {
+    res.sendFile(__dirname + '../client/build/index.html')
+  })
+}
