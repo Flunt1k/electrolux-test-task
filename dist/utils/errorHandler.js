8a392a78c46cdb8f30374a9294d3da9738e1fbf9
Flunt1k <chvp00@gmail.com>
2020-10-12 03:14:15 +0300

    release done

--- dist/utils/errorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(err, res) {
-    res.status(500).json({
-        status: 'error',
-        message: err.message ? err.message : err
-    });
-}
-exports.default = default_1;
